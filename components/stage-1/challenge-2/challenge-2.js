10a7e00ac56f9e1f455258f5394fdb162176ce03
jarp <jarp@outlook.com>
2025-03-06 23:48:01 -0300

    chore: desafio 2 con box model adaptativo

---
+++ components/stage-1/challenge-2/challenge-2.js
@@ -0,0 +1,7 @@
+testAnalyzer(
+	"card1",
+	"components/stage-1/challenge-2/challenge-2.html",
+	"components/stage-1/challenge-2/challenge-2.css",
+	"background-color"
+);
+console.log("Challenge 2 started");
